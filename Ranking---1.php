78f342b5f0fa59101697cab56894512f6997ab60
ShimoLink <72894488+ShimoLink@users.noreply.github.com>
2021-12-10 22:53:48 +0000

    Add files via upload

---
+++ Ranking---1.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="​&nbsp;Check-in Easy​, A form to fill is all that is needed for attendance to be checked, Quick and Reliable​​, ​ Visit our Community​, We have a large community to&nbsp;help you connected and engaged​">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Ranking | Student Attendance Check-in System</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="Ranking---1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <link rel="icon" href="images/favicon.png">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+    <!--JAVASCRIPT-->
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Ranking ",
+		"logo": "images/R.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Ranking - 1">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+  </head>
+
+
+<!--BODY OF PAGE-->
+  <body class="u-body u-white">
+
+    <!--HEADER-->
+    <header class="u-clearfix u-header u-header" id="sec-8131"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+
+         <!--LOGO-->
+        <a href="Ranking---1.php" data-page-id="78418895" class="u-image u-logo u-image-1" data-image-width="980" data-image-height="980">
+          <img src="images/R.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <a href="Login-Page.php" data-page-id="1563010219" class="u-border-none u-btn u-button-style u-hover-palette-1-dark-2 u-btn-1">LOG IN</a>
+        <a href="Registration.php" data-page-id="617007408" class="u-black u-border-2 u-btn u-button-style u-hover-palette-1-dark-1 u-btn-2"><span class="u-icon u-text-white"><svg class="u-svg-content" viewBox="0 0 58 58" x="0px" y="0px" style="width: 1em; height: 1em;"><path style="fill:currentColor;" d="M51,49.957c0-3.038-1.717-5.816-4.434-7.174l-9.552-4.776C35.78,37.39,35,36.128,35,34.748V31
+	c0.268-0.305,0.576-0.698,0.904-1.162c1.302-1.838,2.286-3.861,2.969-5.984C40.098,23.477,41,22.345,41,21v-4
+	c0-0.88-0.391-1.667-1-2.217V9c0,0,1.187-9-11-9C16.812,0,18,9,18,9v5.783c-0.609,0.55-1,1.337-1,2.217v4
+	c0,1.054,0.554,1.981,1.383,2.517C19.382,27.869,22,31,22,31v3.655c0,1.333-0.728,2.56-1.899,3.198L11.18,42.72
+	C8.603,44.125,7,46.826,7,49.761V52c0,0,0,0.657,0,1c0,2.761,9.85,5,22,5s22-2.239,22-5c0-0.343,0-1,0-1V49.957z"></path></svg><img></span>&nbsp;SIGN UP FOR FREE
+        </a>
+      </div></header>
+
+      <!--FIRST SECTION -->
+    <section class="u-clearfix u-white u-section-1" id="sec-dc33">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-14 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-container-style u-layout-cell u-left-cell u-shape-rectangle u-size-36 u-size-xs-60 u-layout-cell-1" src="">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-align-center u-custom-font u-font-raleway u-text u-text-palette-1-base u-text-1"> &nbsp;Check-in Easy<span style="font-weight: 700;"></span>
+                  </h2>
+                  <h2 class="u-align-center u-custom-font u-font-raleway u-subtitle u-text u-text-palette-2-light-1 u-text-2">&nbsp;<span class="u-icon"></span>&nbsp;An auto-generated form to fill is all that is needed for attendance to be checked
+                  </h2>
+                  <a href="Registration.php" data-page-id="617007408" class="u-border-none u-btn u-btn-round u-button-style u-gradient u-none u-radius-50 u-text-body-alt-color u-btn-1">Let's get started</a>
+                </div>
+              </div>
+              <div class="u-align-center u-container-style u-layout-cell u-right-cell u-size-24 u-size-xs-60 u-layout-cell-2" src="">
+                <div class="u-container-layout u-container-layout-2" src="">
+                  <div class="u-image u-image-circle u-image-1" data-image-width="1280" data-image-height="1280"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+     <!--SECOND SECTION-->
+    <section class="u-clearfix u-gradient u-section-2" id="sec-ea63">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-container-style u-layout-cell u-left-cell u-size-36 u-size-xs-60 u-layout-cell-1" src="">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-align-center u-custom-font u-font-raleway u-text u-text-body-alt-color u-text-1">Quick and Reliable<span style="font-weight: 700;">
+                      <span style="font-weight: 400;"></span>
+                    </span>
+                  </h2>
+                  <p class="u-align-center u-text u-text-body-alt-color u-text-2">Security is checked to make sure no outsiders tamper with the registration.</p>
+                  <a href="Registration.php" data-page-id="617007408" class="u-black u-border-none u-btn u-btn-round u-button-style u-hover-grey-60 u-radius-50 u-btn-1">Let's get started&nbsp;<span class="u-icon"></span>
+                  </a>
+                </div>
+              </div>
+              <div class="u-align-center u-container-style u-layout-cell u-right-cell u-size-24 u-size-xs-60 u-layout-cell-2" src="">
+                <div class="u-container-layout u-valign-bottom-xl u-container-layout-2" src="">
+                  <div class="u-image u-image-circle u-image-1" data-image-width="1280" data-image-height="1280"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--LAST SECTION-->
+    <section class="u-clearfix u-white u-section-3" id="sec-a2a5">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-gutter-14 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-container-style u-layout-cell u-left-cell u-shape-rectangle u-size-37 u-size-xs-60 u-layout-cell-1" src="">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-align-center u-custom-font u-font-raleway u-text u-text-palette-1-base u-text-1"> Visit our Community<span style="font-weight: 700;"></span>
+                  </h2>
+                  <h2 class="u-align-center u-custom-font u-font-raleway u-subtitle u-text u-text-palette-2-light-1 u-text-2">&nbsp;<span class="u-icon"></span>&nbsp;We have a large community to&nbsp;<br>help you get connected and engaged<span style="font-size: 1.875rem;"></span>
+                  </h2>
+                  <a href="Registration.php" data-page-id="617007408" class="u-border-none u-btn u-btn-round u-button-style u-gradient u-none u-radius-50 u-text-body-alt-color u-btn-1">Learn more</a>
+                </div>
+              </div>
+              <div class="u-align-center u-container-style u-layout-cell u-right-cell u-size-23 u-size-xs-60 u-layout-cell-2" src="">
+                <div class="u-container-layout u-valign-middle-xl u-container-layout-2" src="">
+                  <img class="u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-1" src="images/default-people-icon-over-purple-circle-and-white-vector-29705161.jpg" data-image-width="999" data-image-height="1080">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <style class="u-overlap-style">.u-overlap:not(.u-sticky-scroll) .u-header {
+background-color: #ffffff !important
+}</style>
+
+<!--FOOTER-->
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-c163">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">All Rights Reserved | Terms of Use | Privacy | Trust Center | Cookie Settings</p>
+        <p class="u-text u-text-2">English&nbsp;&nbsp;</p>
+      </div>
+    </footer>
+
+
+  </body>
+
+
+</html>
